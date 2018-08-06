b4aea2fb1d0487a65b4701663a06a65fe848c505
frankfanwei@outlook.com <fanwei@fanweideMacBook-Pro.local>
2018-08-06 16:57:07 +0800

    add clean

--- DataCleaning.py
+++ DataCleaning.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import pandas as pd
-df = pd.read_csv('/Users/fanwei/Downloads/智慧旅游-旅馆信息数据集1.csv')
+df = pd.read_csv('/SVM_ENG/智慧旅游-旅馆信息数据集1.csv')
 
 a= np.array(df)
 c=[]
@@ -15,7 +15,7 @@ for i in range(rows):
             break
 a=np.delete(a,c,axis=0)
 pd_data=pd.DataFrame(a,columns=range(cols))
-pd_data.to_csv('/Users/fanwei/Downloads/pd_data.csv')
+pd_data.to_csv('/SVM_ENG/智慧旅游-旅馆信息数据集(修改).csv')
 
 
 
